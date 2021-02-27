f2043a8e3b65a01e8bd46f85091b527b2fd1f342
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Add /records/email business logic

--- src/example_clojure_api/http/records.clj
+++ src/example_clojure_api/http/records.clj
@@ -1,11 +1,26 @@
-(ns example-clojure-api.http.records)
+(ns example-clojure-api.http.records
+  (:require
+    [example-clojure-api.parse :as parse]
+    [example-clojure-api.comparator :refer [by-email-desc-lastname-asc]]
+    [ring.util.response :as response]))
 
 (defonce records (atom []))
 
-(defn get-sorted-by-email
+(defn by-fn
+  "Get sorted records, sorted by fn, where fn is a comparator."
+  [f]
+  (let [results (->> records
+                     deref
+                     (sort f)
+                     (mapv #(update % :dob parse/date->str)))]
+    (response/response
+      {:size    (count results)
+       :results results})))
+
+(defn by-email
   "Get a list of records sorted by email."
-  [arglist]
-  )
+  []
+  (by-fn by-email-desc-lastname-asc))
 
 (defn get-sorted-by-birthdate
   "Get a list of records sorted by birthdate."
--- src/example_clojure_api/http/routes.clj
+++ src/example_clojure_api/http/routes.clj
@@ -1,12 +1,13 @@
 (ns example-clojure-api.http.routes
   (:require
+    [example-clojure-api.http.records :as records]
     [compojure.core :refer [context defroutes GET POST]]
     [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
     [ring.middleware.defaults :refer [api-defaults wrap-defaults]]))
 
 (defroutes routes
   (context "/records" []
-    (GET "/email" [] (response/response {:email "email"}))
+    (GET "/email" [] (records/by-email))
     (GET "/birthdate" [] "birthdate")
     (GET "/name" [] "name")
     (POST "/" [] "data")))
