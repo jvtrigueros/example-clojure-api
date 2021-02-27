f373acbdf32e5fa9aa2feafc55559c51a3e2dff7
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Add custom comparator fns to sort data

--- src/example_clojure_api/comparator.clj
+++ src/example_clojure_api/comparator.clj
@@ -1,8 +1,20 @@
-(ns example-clojure-api.comparator)
+(ns example-clojure-api.comparator
+  "Custom comparator functions.
+   Source: https://clojure.org/guides/comparators")
 
 (defn by-email-desc-lastname-asc
-  "Sort by Email descending, then LastName ascending
-   Source: https://clojure.org/guides/comparators"
+  "Compare by Email descending, then LastName ascending"
   [x y]
   (compare [(:email y) (:last-name x)]
            [(:email x) (:last-name y)]))
+
+(defn by-lastname-desc
+  "Compare by LastName descending"
+  [x y]
+  (compare [(:email y) (:last-name x)]
+           [(:email x) (:last-name y)]))
+
+(defn by-dob-asc
+  "Compare by DoB ascending"
+  [x y]
+  (compare (:dob y) (:dob x)))
