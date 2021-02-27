6f49a81a5b1e49da500322a371df5a1e405864be
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Add compojure, http-kit, and ring dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject example-clojure-api "0.1.0"
   :description "Example of a Clojure API as both a CLI and HTTP app."
-  :dependencies [[org.clojure/clojure "1.10.2"]
-                 [org.clojure/data.csv "1.0.0"]]
+  :dependencies [[compojure "1.6.2"]
+                 [http-kit "2.5.3"]
+                 [org.clojure/clojure "1.10.2"]
+                 [org.clojure/data.csv "1.0.0"]
+                 [ring "1.9.1"]]
   :main ^:skip-aot example-clojure-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot      :all
