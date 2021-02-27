7af4b4f968afcb73b3add5f26c22098d526ecce3
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Add routes namespace, defines available routes and provides app handler

--- src/example_clojure_api/http/routes.clj
+++ src/example_clojure_api/http/routes.clj
@@ -1,1 +1,27 @@
-(ns example-clojure-api.http.routes)
+(ns example-clojure-api.http.routes
+  (:require
+    [compojure.core :refer [context defroutes GET POST]]
+    [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+    [ring.middleware.defaults :refer [api-defaults wrap-defaults]]))
+
+(defroutes routes
+  (context "/records" []
+    (GET "/email" [] (response/response {:email "email"}))
+    (GET "/birthdate" [] "birthdate")
+    (GET "/name" [] "name")
+    (POST "/" [] "data")))
+
+(defn debug-middleware
+  "Dummy middleware for quick inspecting of values in the middleware chain."
+  [handler]
+  (fn [request]
+    (println (:headers request))
+    (let [response (handler request)]
+      (println (:headers response))
+      response)))
+
+(defn create-app-handler
+  []
+  (-> routes
+      (wrap-json-response)
+      (wrap-defaults api-defaults)))
