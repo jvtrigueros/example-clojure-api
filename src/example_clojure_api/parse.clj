228c7789f03285bd3e8a20c570d1ed2b84066c48
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Handle Exceptions in parse-line

--- src/example_clojure_api/parse.clj
+++ src/example_clojure_api/parse.clj
@@ -29,14 +29,18 @@
 (defn parse-line
   "Given an input line, split line into a map with keys :last-name, :first-name, :email, :favorite-color, and :dob."
   [line]
-  (let [re  #"\s*(\||,|\s)\s*"                              ;; Zero or more spaces followed by pipe(|), comma(,), or space( )
-        [last-name first-name email favorite-color dob :as result] (str/split line re)
-        dob (str->date dob)]
-    (when (and (= column-count
-                  (count result))
-               dob)
-      {:last-name      last-name
-       :first-name     first-name
-       :email          email
-       :favorite-color favorite-color
-       :dob            dob})))
+  (try
+    (let [re  #"\s*(\||,|\s)\s*"                         ;; Zero or more spaces followed by pipe(|), comma(,), or space( )
+          [last-name first-name email favorite-color dob :as result] (str/split line re)
+          dob (str->date dob)]
+      (when (and (= column-count
+                    (count result))
+                 dob)
+        {:last-name      last-name
+         :first-name     first-name
+         :email          email
+         :favorite-color favorite-color
+         :dob            dob}))
+    (catch Exception e_
+      ;; Ignore Exception, but return nil instead of bubbling up
+      (println "Failed to parse line" line))))
