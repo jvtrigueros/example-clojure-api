b9fe11a0b669f495cbc3321dee7e2e923f53db63
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Stub out business logic functions for each endpoint

--- src/example_clojure_api/http/records.clj
+++ src/example_clojure_api/http/records.clj
@@ -1,3 +1,23 @@
 (ns example-clojure-api.http.records)
 
 (defonce records (atom []))
+
+(defn get-sorted-by-email
+  "Get a list of records sorted by email."
+  [arglist]
+  )
+
+(defn get-sorted-by-birthdate
+  "Get a list of records sorted by birthdate."
+  [arglist]
+  )
+
+(defn get-sorted-by-name
+  "Get a list of records sorted by last name."
+  [arglist]
+  )
+
+(defn create-record!
+  "Create a new record and add to database."
+  [arglist]
+  )
