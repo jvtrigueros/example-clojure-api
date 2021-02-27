cdf4678bffe63d7a66b6118316bda9d9d9d3f6d2
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Add clojure.data.csv dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject example-clojure-api "0.1.0"
   :description "Example of a Clojure API as both a CLI and HTTP app."
-  :dependencies [[org.clojure/clojure "1.10.2"]]
+  :dependencies [[org.clojure/clojure "1.10.2"]
+                 [org.clojure/data.csv "1.0.0"]]
   :main ^:skip-aot example-clojure-api.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
+  :profiles {:uberjar {:aot      :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ src/example_clojure_api/cli.clj
@@ -0,0 +1,1 @@
+(ns example-clojure-api.cli)
