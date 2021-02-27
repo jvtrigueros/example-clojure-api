7fa46aee03c2d1ae5ad640e2caef57a419191661
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Update warning message when LocalDate cannot be parsed

--- src/example_clojure_api/parse.clj
+++ src/example_clojure_api/parse.clj
@@ -15,7 +15,7 @@
     (LocalDate/parse s date-formatter)
     (catch Exception e_
       ;; Ignore Exception, but return nil instead of bubbling up the Exception
-      (println "Couldn't parse" s))))
+      (println "Failed to parse" s "as LocalDate!"))))
 
 (defn parse-line
   "Given an input line, split line into a map with keys :last-name, :first-name, :email, :favorite-color, and :dob."
@@ -31,11 +31,3 @@
        :email          email
        :favorite-color favorite-color
        :dob            dob})))
-
-(comment
-  (require
-    '[clojure.java.io :as io])
-
-  (let [test-data   (-> "./resources/test-data.psv" io/file slurp str/split-lines)
-        parsed-data (map parse-line test-data)]
-    (take 10 parsed-data)))
