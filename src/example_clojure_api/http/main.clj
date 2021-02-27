9f13f8e3d3d34a35552be187751c21ff55fd7d12
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Create HTTP server entrypoint

--- src/example_clojure_api/http/main.clj
+++ src/example_clojure_api/http/main.clj
@@ -1,1 +1,21 @@
-(ns example-clojure-api.http.main)
+(ns example-clojure-api.http.main
+  (:require
+    [example-clojure-api.http.routes :as routes]
+    [org.httpkit.server :refer [run-server]]))
+
+(defonce server (atom nil))
+
+(defn stop-server
+  "Stops a running server, used in development."
+  []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn -main [& args]
+  (let [handler (routes/create-app-handler)
+        port    (Integer/parseInt (or (System/getenv "PORT")
+                                      "8080"))]
+    (run-server
+      handler
+      {:port port})))
