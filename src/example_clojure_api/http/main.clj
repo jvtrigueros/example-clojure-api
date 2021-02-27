f6b0d3b6bf9b8cf80e62e287e93ece664bc6805f
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-02-26 18:52:59 -0800

    Add rich comment section for testing application with REPL

--- src/example_clojure_api/http/main.clj
+++ src/example_clojure_api/http/main.clj
@@ -12,10 +12,45 @@
     (@server :timeout 100)
     (reset! server nil)))
 
-(defn -main [& args]
+(defn -main
+  [& args]
   (let [handler (routes/create-app-handler)
         port    (Integer/parseInt (or (System/getenv "PORT")
                                       "8080"))]
     (run-server
       handler
       {:port port})))
+
+(comment
+  ;; Interactive Testing playground
+
+  (require '[org.httpkit.client :as http]
+           '[example-clojure-api.http.records :refer [records]]
+           '[clojure.java.io :as io]
+           '[clojure.string :as str])
+  (import '(java.time LocalDate))
+
+  (reset! records
+          [{:last-name "z" :email "a@b.com" :dob (.minusDays (LocalDate/now) 0)}
+           {:last-name "z" :email "z@b.com" :dob (.minusDays (LocalDate/now) 1)}
+           {:last-name "a" :email "a@b.com" :dob (.minusDays (LocalDate/now) 2)}
+           {:last-name "a" :email "z@b.com" :dob (.minusDays (LocalDate/now) 3)}])
+
+  (do
+    (stop-server)
+    (reset! server
+            (run-server
+              (routes/create-app-handler)
+              {:port 3000})))
+
+  @(http/get "http://localhost:3000/records/email"
+             {:headers {"Content-Type" "application/json"}})
+
+  @(http/post "http://localhost:3000/records"
+              {})
+
+  (let [lines (-> "test-data.csv" io/resource slurp str/split-lines)]
+    (doseq [line (take 10 lines)]
+      @(http/post
+         "http://localhost:3000/records"
+         {:body line}))))
